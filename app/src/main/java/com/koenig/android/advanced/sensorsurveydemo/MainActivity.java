c0395b58182928e0dd62ecc3ced59d0b6a6b5477
Lalit Singh <lalitsinghdrafter047@gmail.com>
2018-08-01 22:44:37 +0530

    Initial commit

---
+++ app/src/main/java/com/koenig/android/advanced/sensorsurveydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.koenig.android.advanced.sensorsurveydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
